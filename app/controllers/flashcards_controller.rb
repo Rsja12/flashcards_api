08fcd0ee0ceb1bca3fd751b29cb2563452166379
Rsja12 <asocarras10@gmail.com>
2020-01-12 15:06:43 -0500

    adds index, show, create, and update actions to flashcards_controller

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,2 +1,40 @@
 class FlashcardsController < ApplicationController
+
+    def index
+        flashcards = Flashcard.all
+        render json: flashcards
+    end
+
+    def show
+        flashcard = Flashcard.find_by(id: params[:id])
+        if flashcard
+            render json: flashcard
+        else
+            render json: { message: 'Flashcard not found' }
+        end
+    end
+
+    def create
+        flashcard = flashcard.new(flashcard_params)
+        if flashcard.save
+            render json: flashcard
+        else
+            render json: { message: 'Flashcard not created' }
+        end
+    end
+
+    def update
+        flashcard = Topic.find_by(id: params[:id])
+        flashcard.update(flashcard_params)
+        
+        render json: flashcard
+    end
+
+
+    private
+
+    def flashcard_params
+        params.require(:flashcard).permit(:name, :description)
+    end
+
 end
