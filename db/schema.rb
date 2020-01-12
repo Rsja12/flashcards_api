2ae651448ef89128ba55626f8794e2e1520a9a0e
Rsja12 <asocarras10@gmail.com>
2020-01-12 14:58:57 -0500

    adds flashcard resource. Adds relationship between topic and flashcards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_190458) do
+ActiveRecord::Schema.define(version: 2020_01_12_195741) do
+
+  create_table "flashcards", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "name"
